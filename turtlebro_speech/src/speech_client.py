98eb852c8f953bdd4a9cc93c7641680bfe829884
Ubercola <cola@cactus.ru>
2022-08-11 15:33:48 +0300

    add speech client demo

---
+++ turtlebro_speech/src/speech_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from turtlebro_speech.srv import Speech, SpeechResponse, SpeechRequest 
+
+
+class SpeechClient():
+
+    def __init__(self) -> None:
+
+        self.speech_service = rospy.ServiceProxy('festival_speech', Speech)
+        rospy.loginfo(f"Waiting for festival_speech service")
+        self.speech_service.wait_for_service()
+        rospy.loginfo(f"Have festival_speech service")  
+
+    def say(self, text):
+
+        result: SpeechResponse = self.speech_service.call(SpeechRequest(data = text))
+
+rospy.init_node('festival_speech_client')
+
+speech = SpeechClient()
+speech.say("Привет мир")
+
+rospy.sleep(2)
